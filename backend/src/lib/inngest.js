2f1fcc6520b6e265b92f6189559dac18ab380502
ishfaq24 <ib8543092>
2025-11-03 11:04:00 +0530

    fixed some import errors

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,35 +1,34 @@
-import { inngest } from "inngest";
-import { connectDB } from "./db";
-import user from "./models/user.js";
+import { Inngest } from "inngest";
+import { connectDB } from "./db.js";
+import User from "../models/User.js";
 
-export const inngest = new inngest({ id: "AlgoArena"});
+export const inngest = new Inngest({ id: "AlgoArena" });
 
 const syncUser = inngest.createFunction(
-    {id: "sync-user", name: "Sync User to DB"},
-    {event: "clerk/user.created"},
-    async ({event})=>{
-        await connectDB();
-        const {id, email_addresses, first_name,last_name,image_url} = event.data;
+  { id: "sync-user", name: "Sync User to DB" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    await connectDB();
+    const { id, email_addresses, first_name, last_name, image_url } = event.data;
 
-        const newUser ={
-            clerkId: id,
-            email: email_addresses[0].email_address,
-            name: `${first_name} ${last_name}`,
-            profileImage: image_url,
-        };
-        await User.create(newUser);
-    }
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      name: `${first_name} ${last_name}`,
+      profileImage: image_url,
+    };
+    await User.create(newUser);
+  }
 );
 
 const deleteUserFromDB = inngest.createFunction(
-    {id: "delete-user", name: "Delete User from DB"},
-    {event: "clerk/user.deleted"},
-    async ({event})=>{
-        await connectDB();
-        const {id} = event.data;
-        await User.findOneAndDelete
-        ({clerkId: id});
-    }       
+  { id: "delete-user", name: "Delete User from DB" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    await connectDB();
+    const { id } = event.data;
+    await User.findOneAndDelete({ clerkId: id });
+  }
 );
 
 export const functions = [syncUser, deleteUserFromDB];
