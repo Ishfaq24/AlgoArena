39cba78778ade054237ff493d398b46c440ffe8b
Ishfaq Nazir <121862516+Ishfaq24@users.noreply.github.com>
2025-11-04 12:50:26 +0530

    Fix import statement for env.js
    Updated import statement for environment variables to include the correct file extension.

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,5 +1,5 @@
 import {StreamChat} from 'stream-chat';
-import { ENV } from './env';
+import { ENV } from './env.js';
 
 const apiKey = ENV.STREAM_API_KEY;
 const apiSecret = ENV.STREAM_API_SECRET;
@@ -20,7 +20,6 @@ export const upsertStreamUser = async(userData)=>{
         return userData;
     }catch(error){
         console.error('Error upserting Stream user:', error);
-        throw error;
     }
 };
 
@@ -30,7 +29,6 @@ export const deleteStreamUser = async(userId)=>{
         console.log(`Stream user with ID ${userId} deleted successfully.`);
     }catch(error){
         console.error('Error deleting Stream user:', error);
-        throw error;
     }
 };
 
