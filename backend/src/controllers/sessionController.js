68f5903ac2a0aad281febdfe2b4ddef1ccd67bdd
ishfaq24 <ib8543092>
2025-11-05 14:38:57 +0530

    refactoring session.js

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -96,8 +96,17 @@ export async function joinASession(req, res) {
             return res.status(404).json({ message: 'Session not found' });
         }
 
+        if(!session.status !== "active"){
+            return res.status(400).json({ message: 'Cannot join a non-active session' });
+        }
+
+        if(session.host.toString() === userId.toString()){
+            return res.status(400).json({ message: 'Host cannot join their own session as participant' });
+        }
+
+
         if (session.participant) {
-            return res.status(404).json({ message: 'Session already has a participant' });
+            return res.status(409).json({ message: 'Session already has a participant' });
         }
 
         session.participant = userId;
@@ -130,13 +139,15 @@ export async function endSession(req, res) {
         if (session.status === 'Completed') {
             return res.status(400).json({ message: 'Session is already completed' });
         }
-        session.status = 'Completed';
-        await session.save();
+        
 
         //delete stream video call
         await streamClient.video.call("default", session.callId).delete({hard:true});
 
         await chatClient.channel("messaging", session.callId).delete({hard:true});
+
+        session.status = 'Completed';
+        await session.save();
 
         res.status(200).json({ session,message: 'Session ended successfully' });
 
